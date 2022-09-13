57707616ed43ddda2400e0f09a44f6b291b4d9a1
Bohdan Pukhalskyi <bodik90@gmail.com>
2022-09-13 17:05:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
+//13/09/2022
 // var number = function(busStops){
 //   let passengers = 0;
-//     for (let stop of busStops) { 
+//     for (let stop of busStops) {
 //     passengers += stop[0] - stop[1]
 //     console.log(stop);
 //   }
 // console.log(passengers);
-
-
 // }
 
-
 // const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
-
-
 // number([[10,0],[3,5],[5,8]]) //5
